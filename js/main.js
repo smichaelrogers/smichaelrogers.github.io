0b5b8b2cc3dc4db94fa1fc2b4133abc18dc2a57c
smichaelrogers <mrogerssn@gmail.com>
2015-08-26 04:33:49 -0400

    address flash of unstyled content issue

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 (function (window) {
-	$(function() {
+	$("body").css("display", "none").css("opacity", "0");
+	$(function () {
 		$("#header").height($(window).height());
 		$("#spacing-top").height($(window).height() * 0.3);
+		window.setTimeout(function () {
+			$("body").show();
+			$("body").css("opacity", "1");
+		}, 200);
 	});
 
 }(window));
